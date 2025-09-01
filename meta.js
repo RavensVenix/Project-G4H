0a0258d6c81ad9f98b414c84d5c69d247886a37f
!g4h∅mx4. <ravensvenix@gmail.com>
2025-09-01 15:01:38 +0800

    Up Version to 3.0

--- meta.js
+++ meta.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Project G4H
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      3.0
 // @description  Mem-bypass segala iklan, pop-up, timer, shortlink dan masih banyak lagi!
 // @author       @g4hmx0
 // @match        *://*/*
-// @grant        none
+// @grant        GM_setClipboard
 // ==/UserScript==
